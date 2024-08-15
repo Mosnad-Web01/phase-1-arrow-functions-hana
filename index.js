e3dae8574e348db809a0d974f97c93b6a26a99d8
Hana Alhemyari <hana.saif.1978@gmail.com>
2024-08-15 07:02:22 +0300

    Update index.js
    .

---
+++ index.js
@@ -0,0 +1,14 @@
+// دالة للتقسيم بين عددين
+const divide = function(a, b) {
+    return a / b;
+  };
+  
+  // دالة سهمية تأخذ معاملًا واحدًا وترفعه للقوة الثانية
+  const square = x => x * x;
+  
+  // دالة سهمية تجمع بين معاملين
+  const add = (a, b) => a + b;
+
+  console.log(divide(10,3))  ;
+  console.log(square(10,3))  ;
+  console.log(add(10,3))  ;
